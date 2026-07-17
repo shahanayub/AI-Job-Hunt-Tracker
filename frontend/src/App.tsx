0fd5dd120025613bb2bd1a04a2d42d5cbb9a7dc5
Shahan Ayub <shahanayub39@gmail.com>
2026-07-17 15:07:56 +0500

    feat: add react router  and dashboard page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features' 
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   const features = [
@@ -23,14 +25,23 @@ function App() {
   },
 ]
   return (
-    <>
-      <Navbar />
-      <Hero
-  title="Land Your Dream Job with AI"
-  subtitle="Track applications, analyze resumes, and discover the best opportunities using AI."
-/>
-      <Features features={features} />
-    </>
+    <Routes>
+  <Route
+    path="/"
+    element={
+      <>
+        <Navbar />
+        <Hero
+          title="Land Your Dream Job with AI"
+          subtitle="Track applications, analyze resumes, and discover the best opportunities using AI."
+        />
+        <Features features={features} />
+      </>
+    }
+  />
+
+  <Route path="/dashboard" element={<Dashboard />} />
+</Routes>
   )
 }
 
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to your AI Job Hunt Tracker Dashboard!</p>
+    </div>
+  )
+}
+
+export default Dashboard
