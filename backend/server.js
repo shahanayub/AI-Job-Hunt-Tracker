9ce5431322e7a95fcdcfd4603afd431f80766306
Shahan Ayub <shahanayub39@gmail.com>
2026-07-28 21:04:02 +0500

    feat: implement add and delete job functionality

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 const jobs = []
@@ -37,10 +39,6 @@ app.post('/jobs', (req, res) => {
 
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
-
 app.delete('/jobs/:id', (req, res) => {
 
   const id = Number(req.params.id)
@@ -50,8 +48,39 @@ app.delete('/jobs/:id', (req, res) => {
   jobs.length = 0
   jobs.push(...updatedJobs)
 
+  console.log(jobs)
+
   res.json({
     message: "Job deleted successfully!"
   })
 
+})
+
+app.put('/jobs/:id', (req, res) => {
+
+  const id = Number(req.params.id)
+
+  const job = jobs.find(job => job.id === id)
+
+  if (!job) {
+    return res.status(404).json({
+      message: "Job not found"
+    })
+  }
+
+  job.company = req.body.company
+  job.position = req.body.position
+  job.status = req.body.status
+
+  console.log(jobs)
+
+  res.json({
+    message: "Job updated successfully!",
+    job
+  })
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
